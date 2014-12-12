e1d232eefc5005dfd6fef5f45c60957051a2a096
Maxim Dybarsky <maxim.dybarskyy@gmail.com>
2014-12-12 14:28:59 +0100

    Add launcher activity

---
+++ src/main/java/dmax/words/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package dmax.words.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import dmax.words.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.a_main);
+    }
+}
