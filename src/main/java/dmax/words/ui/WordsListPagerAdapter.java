cfdf274a0f867ad6593eb9fb512781b5ffa771b2
Maxim Dybarsky <maxim.dybarskyy@gmail.com>
2014-12-19 16:39:25 +0100

    Implement Card switcher (with dummy data)

--- src/main/java/dmax/words/ui/WordsListPagerAdapter.java
+++ src/main/java/dmax/words/ui/WordsListPagerAdapter.java
@@ -3,38 +3,41 @@ package dmax.words.ui;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
-import android.graphics.Color;
 import android.support.v4.view.PagerAdapter;
 import android.support.v7.widget.CardView;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import dmax.words.R;
+import dmax.words.domain.Language;
+import dmax.words.domain.Link;
+import dmax.words.domain.Word;
 
 /**
  * Created by Maxim Dybarsky | maxim.dybarskyy@gmail.com
  * on 18.12.14 at 12:38
  */
-public class WordsListPagerAdapter extends PagerAdapter implements View.OnTouchListener {
+public class WordsListPagerAdapter extends PagerAdapter {
 
-    private Object[] data = new Object[] {
-            new Object(),
-            new Object(),
-            new Object(),
-            new Object(),
-            new Object()
+    private String[][] data = new String[][] {
+            {"брехати", "вирішити", "пенсія", "різати"},
+            {"kłamać", "zdecydować", "emerytura", "kroić"},
     };
+
     private Context context;
+    private CardStateSwitcher switcher;
 
     public WordsListPagerAdapter(Context context) {
         this.context = context;
+        this.switcher = new CardStateSwitcher();
     }
 
     @Override
     public int getCount() {
-        return data.length;
+        return data[0].length;
     }
 
     @Override
@@ -44,14 +47,37 @@ public class WordsListPagerAdapter extends PagerAdapter implements View.OnTouchL
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        Object key = data[position];
-
         CardView card = (CardView) View.inflate(context, R.layout.v_wordslist_item, null);
-        card.setTag(key);
-        card.setOnTouchListener(this);
+
+        CardViewHolder holder = new CardViewHolder();
+        holder.originalViewGroup = card.findViewById(R.id.original);
+        holder.translationViewGroup = card.findViewById(R.id.translation);
+        holder.originalTextView = (TextView) card.findViewById(android.R.id.text1);
+        holder.translationTextView = (TextView) card.findViewById(android.R.id.text2);
+
+        holder.originalViewGroup.bringToFront();
+
+        {
+            Word word1 = new Word();
+            word1.setLanguage(Language.UKRAINIAN);
+            word1.setData(data[0][position]);
+
+            Word word2 = new Word();
+            word2.setLanguage(Language.POLISH);
+            word2.setData(data[1][position]);
+
+            holder.originalWord = word1;
+            holder.translationWord = word2;
+            holder.currentLanguage = word1.getLanguage();
+
+            holder.originalTextView.setText(word1.getData());
+        }
+
+        card.setTag(holder);
+        card.setOnTouchListener(switcher);
         container.addView(card);
 
-        return key;
+        return holder;
     }
 
     @Override
@@ -60,26 +86,64 @@ public class WordsListPagerAdapter extends PagerAdapter implements View.OnTouchL
         container.removeView(view);
     }
 
-    @Override
-    public boolean onTouch(final View v, MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_UP) {
+    //~
 
-            View translation = v.findViewById(R.id.translation);
-            final View original = v.findViewById(R.id.original);
-            int cx = (original.getLeft() + original.getRight()) / 2;
-            int cy = (original.getTop() + original.getBottom()) / 2;
+    private static class CardViewHolder {
 
-            translation.setVisibility(View.VISIBLE);
-            Animator anim = ViewAnimationUtils.createCircularReveal(translation, cx, cy, 0, translation.getHeight());
-            anim.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    original.setVisibility(View.INVISIBLE);
-                    ((CardView) v).setCardBackgroundColor(context.getResources().getColor(R.color.yellow));
+        boolean isTranslationState;
+
+        Link link;
+        Word originalWord;
+        Word translationWord;
+        Language currentLanguage;
+
+        View originalViewGroup;
+        View translationViewGroup;
+        TextView originalTextView;
+        TextView translationTextView;
+    }
+
+    private static class CardStateSwitcher implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(final View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                CardView card = (CardView) v;
+                final CardViewHolder holder = (CardViewHolder) v.getTag();
+
+                View viewGroup;
+                TextView textView;
+                String text;
+                if (holder.isTranslationState) {
+                    viewGroup = holder.originalViewGroup;
+                    textView = holder.originalTextView;
+                    text = holder.originalWord.getData();
+                } else {
+                    viewGroup = holder.translationViewGroup;
+                    textView = holder.translationTextView;
+                    text = holder.translationWord.getData();
                 }
-            });
-            anim.start();
+
+                textView.setText(text);
+                viewGroup.bringToFront();
+                prepareTransition(viewGroup, event).start();
+
+                holder.isTranslationState = !holder.isTranslationState;
+
+                return true;
+            }
+            return false;
         }
-        return false;
+
+        private Animator prepareTransition(View v, MotionEvent event) {
+            int cx = (int) event.getX();
+            int cy = (int) event.getY();
+
+            float maxX = Math.max(cx, v.getWidth() - cx);
+            float maxY = Math.max(cy, v.getHeight() - cy);
+            float radius = (float) Math.sqrt(Math.pow(maxX, 2) + Math.pow(maxY, 2));
+
+            return ViewAnimationUtils.createCircularReveal(v, cx, cy, 0, radius);
+        }
     }
 }
