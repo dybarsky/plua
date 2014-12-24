9150db71faed1ed612f2882e4134270ee7cadb39
Maxim Dybarskiy <maxim.dybarskyy@gmail.com>
2014-12-24 01:58:45 +0100

    Implement language switching in 'add word' fragment

--- src/main/java/dmax/words/ui/AddWordFragment.java
+++ src/main/java/dmax/words/ui/AddWordFragment.java
@@ -1,5 +1,7 @@
 package dmax.words.ui;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Context;
@@ -26,23 +28,27 @@ import dmax.words.persist.dao.DaoFactory;
  * Created by Maxim Dybarsky | maxim.dybarskyy@gmail.com
  * on 22.12.14 at 17:01
  */
-public class AddWordFragment extends Fragment {
+public class AddWordFragment extends Fragment implements View.OnClickListener {
 
+    public static final int DURATION = 250;
     private ImageView originalFlag;
     private ImageView translationFlag;
+    private ImageView switchFlag;
     private EditText originalText;
     private EditText translationText;
 
     private boolean updateList = false;
+    private Language current = Language.UKRAINIAN;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.f_addword, container, false);
 
-        originalFlag = (ImageView) root.findViewById(R.id.origin_lang);
-        translationFlag = (ImageView) root.findViewById(R.id.transl_lang);
-        originalText = (EditText) root.findViewById(R.id.origin_text);
-        translationText = (EditText) root.findViewById(R.id.transl_text);
+        this.switchFlag = (ImageView) root.findViewById(R.id.switch_lang);
+        this.originalFlag = (ImageView) root.findViewById(R.id.origin_lang);
+        this.translationFlag = (ImageView) root.findViewById(R.id.transl_lang);
+        this.originalText = (EditText) root.findViewById(R.id.origin_text);
+        this.translationText = (EditText) root.findViewById(R.id.transl_text);
 
         return root;
     }
@@ -56,8 +62,15 @@ public class AddWordFragment extends Fragment {
         actionBar.setTitle(R.string.add_word_title);
         setHasOptionsMenu(true);
 
-        originalFlag.setImageResource(R.drawable.ic_ukrainian);
-        translationFlag.setImageResource(R.drawable.ic_polish);
+        this.switchFlag.bringToFront();
+        this.switchFlag.setOnClickListener(this);
+
+        this.originalFlag.setImageResource(current.equals(Language.UKRAINIAN)
+                ? R.drawable.ic_ukrainian
+                : R.drawable.ic_polish);
+        this.translationFlag.setImageResource(current.equals(Language.UKRAINIAN)
+                ? R.drawable.ic_polish
+                : R.drawable.ic_ukrainian);
     }
 
     @Override
@@ -90,15 +103,28 @@ public class AddWordFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onClick(View v) {
+        this.current = current.equals(Language.UKRAINIAN) ? Language.POLISH : Language.UKRAINIAN;
+
+        ObjectAnimator rotate = ObjectAnimator.ofFloat(switchFlag, "rotation", 0, 180);
+        ObjectAnimator moveOriginal = ObjectAnimator.ofFloat(originalFlag, "y", originalFlag.getY(), translationFlag.getY());
+        ObjectAnimator moveTranslation = ObjectAnimator.ofFloat(translationFlag, "y", translationFlag.getY(), originalFlag.getY());
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(rotate, moveOriginal, moveTranslation);
+        set.setDuration(DURATION);
+        set.start();
+    }
+
     private void save() {
         if (check(originalText) || check(translationText)) return;
 
         Word word1 = new Word();
-        word1.setLanguage(Language.UKRAINIAN);
+        word1.setLanguage(current.equals(Language.UKRAINIAN) ? Language.POLISH : Language.UKRAINIAN);
         word1.setData(originalText.getText().toString());
 
         Word word2 = new Word();
-        word2.setLanguage(Language.POLISH);
+        word2.setLanguage(current.equals(Language.UKRAINIAN) ? Language.UKRAINIAN : Language.POLISH);
         word2.setData(translationText.getText().toString());
 
         Link link = new Link();
