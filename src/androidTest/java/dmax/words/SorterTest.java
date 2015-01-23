106a0c300d31edd4f57c520df0061026b5551d25
Maxim Dybarsky <maxim.dybarskyy@gmail.com>
2015-01-23 18:13:38 +0100

    Add sorting test skeleton

---
+++ src/androidTest/java/dmax/words/SorterTest.java
@@ -0,0 +1,34 @@
+package dmax.words;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Maxim Dybarsky | maxim.dybarskyy@gmail.com
+ * on 23.01.15 at 18:00
+ */
+public class SorterTest extends AndroidTestCase {
+
+    public void test_shouldReturnPositive_IfPriorityRightMoreLeft() {
+        
+    }
+
+    public void test_shouldReturnNegative_IfPriorityRightLessLeft() {
+
+    }
+
+    public void test_shouldReturnNegative_IfPrioritySameAndUpdatedRightMoreLeft() {
+
+    }
+
+    public void test_shouldReturnPositive_IfPrioritySameAndUpdatedRightLessLeft() {
+
+    }
+
+    public void test_shouldReturnZero_IfPrioritySameAndUpdatedSame() {
+
+    }
+
+    public void test_shouldSortCollectionPriorityDescendingAndUpdatedAscending() {
+
+    }
+}
